666ff618f9eb5e1a26e8c6dc5726c51ca575ea80
Dave Dong <fudaviddong@Dave-Dongs-Mac-Mini.local>
2016-08-01 20:28:14 -0700

    First Challenge Completed

--- public/app.jsx
+++ public/app.jsx
@@ -17,26 +17,35 @@ var GreeterMessage = React.createClass({
 	}
 });
 
+
+
+
 var GreeterForm = React.createClass({
 	onFormSubmit: function(e){
 		e.preventDefault();
 
-		var name = this.refs.name.value;
+		var updates = {
+			name: this.refs.name.value,
+			message: this.refs.message.value
+		}
 
-		if (name.length > 0){
+		if (updates.name.length > 0){
 			this.refs.name.value = '';
-			this.props.onNewName(name);
+			this.refs.message.value = '';
+			this.props.onNewName(updates);
 		}
 	},
 	render: function(){
 		return (
 			<form onSubmit={this.onFormSubmit}>
-				<input type="text" ref="name"/>
+				<input type="text" ref="name" placeholder="Name"/><br />
+				<textarea type="text" ref="message" placeholder="Message"/><br />
 				<button type="submit">Submit</button>
 			</form>
 		)
 	}
 })
+
 
 var Greeter = React.createClass({
 	// Gets the default props if the variable being passed does not exist.
@@ -52,15 +61,16 @@ var Greeter = React.createClass({
 			name: this.props.name
 		}
 	},
-	handleNewName: function(name) {
+	handleNewName: function(updates) {
 		this.setState({
-			name: name
+			name: updates.name,
+			message: updates.message
 		});
 	},
 	// Renders the HTML in jsx
 	render: function() {
 		var name = this.state.name; //Updateable by the user.
-		var message = this.props.message; //Props can not be updated by ther user.
+		var message = this.state.message; //Props can not be updated by ther user.
 
 		return ( 
 			<div>
